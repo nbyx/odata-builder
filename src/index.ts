7a7349ac80ffd3df2c70973060816ef5321bbcca
Sabit <sabitertan@gmail.com>
2022-08-15 09:25:41 -0400

    Exporting LambdaFilterFields
    For some dynamic filter building scenarios, we should be able to build lambda fields on the fly.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export type {
     QueryFilter,
     FilterFields,
     FilterOperators,
+    LambdaFilterFields
 } from './query-builder/types/filter/query-filter.type';
 export type { CombinedFilter } from './query-builder/types/filter/combined-filter.type';
 export type { Guid } from './query-builder/types/utils/util.types';
